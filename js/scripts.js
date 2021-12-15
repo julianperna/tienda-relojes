3b49e543ae950f96ee524eafd86f2dcf9773e53c
Julián Perna <julianperna@gmail.com>
2021-12-14 21:06:48 -0300

    se modificaron los precios

--- js/scripts.js
+++ js/scripts.js
@@ -200,8 +200,7 @@ class UI {
 		carritoCenter.addEventListener("click", e =>{
 			const target = e.target.closest("span")
 			const targetElement = target.classList.contains("remove__item");
-			console.log(target)
-			console.log(targetElement)
+			
 			if(!target) return
 			if(targetElement){
 				const id = parseInt(target.dataset.id);
